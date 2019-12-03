dea53a87dac89d215bff3432815d09c1a0fcbdfd
kong <1287988804@qq.com>
2019-12-03 10:51:02 +0800

    tijiao

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>HomeContainer</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang='less' scoped>
+</style>
--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,10 @@ import app from './../App.vue'
 import {Header} from 'mint-ui'
 Vue.component(Header.name,Header)
 import './../lib/css/mui.css'
-
+import './../lib/css/icons-extra.css'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+import router from './router.js'
 
 
 
@@ -11,5 +14,6 @@ var vm=new Vue({
     el:'#app',
     render:function(createElement){
         return createElement(app)
-    }
+    },
+    router
 })
